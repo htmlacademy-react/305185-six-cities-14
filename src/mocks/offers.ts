138cdece13c19d742416da775a896b11f54aff17
Nick Shum <nialshum@gmail.com>
2023-11-11 12:36:14 +0500

    feat(mocks): update mocks location's coordinates

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -15,8 +15,8 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.36554,
-      longitude: 4.911976,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     isFavorite: false,
@@ -65,8 +65,8 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.385540000000006,
-      longitude: 4.902976,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     isFavorite: false,
@@ -116,8 +116,8 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.397540000000006,
-      longitude: 4.9099759999999995,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 16
     },
     isFavorite: false,
@@ -174,8 +174,8 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.37454,
-      longitude: 4.881976,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 16
     },
     isFavorite: false,
